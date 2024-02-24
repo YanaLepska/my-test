cc08abc67806d1bbb43ad6d91a7448066f573660
Yana Lepska <lepskayana1624@gmail.com>
2024-02-24 18:48:00 +0200

    code

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -7,9 +7,9 @@ export async function getCategories() {
     axios.defaults.baseURL = 'https://books-backend.p.goit.global/books/category-list';
     const response = await axios.get('');
     const listOfCategories = response.data;
-   const category = listOfCategories.map(item => item.list_name);
+    const category = listOfCategories.map(item => item.list_name);
     const categoryTemplate = category.map(category =>
-        `<li class="category-item">${category}</li>`).join('');
+        `<li class="category-item">${category}</li>`).toSorted().join('');
     categoriesList.insertAdjacentHTML('beforeend', categoryTemplate);
     
     const categoryItem = document.querySelectorAll('.category-item');
@@ -17,17 +17,30 @@ export async function getCategories() {
 
     categoryItem.forEach(item => {
         
-        item.addEventListener('click', ()=>{
+        item.addEventListener('click', (e)=>{
+         e.preventDefault();
+         item.textContent = item.textContent.toUpperCase();
+            if (item.style.color === '#4F2EE8') {
+            item.style.color = '#4F2EE8';
+                item.style.fontWeight = '700';
+                
+        } else {
+            item.style.color = '#4F2EE8';
+            item.style.fontWeight = '700';
 
-            item.textContent = item.textContent.toUpperCase();
-            if (previousItem && previousItem !== item) {
-      
+            // Перевіряємо, чи існує попередній елемент
+            if (previousItem !== null) {
+                // Якщо існує, змінюємо його колір на сірий та жирність шрифту на 400
+                previousItem.style.color = 'rgba(17, 17, 17, 0.6)';
+                previousItem.style.fontWeight = '400';
                 previousItem.textContent = previousItem.getAttribute('data-text');
             }
-            previousItem = item;
-            
+        } previousItem = item;
         });
-        
         item.setAttribute('data-text', item.textContent);
     });
+}
+
+function name(params) {
+    
 }
